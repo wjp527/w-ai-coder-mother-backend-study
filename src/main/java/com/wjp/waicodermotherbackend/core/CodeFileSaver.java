332a0c592f6e7a357dbb68ffa91d061a04f21a4c
wjp <1060638479@qq.com>
2025-12-22 17:44:10 +0800

    实现AI应用生成 1、保存文件

---
+++ src/main/java/com/wjp/waicodermotherbackend/core/CodeFileSaver.java
@@ -0,0 +1,73 @@
+package com.wjp.waicodermotherbackend.core;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import com.wjp.waicodermotherbackend.ai.model.HtmlCodeResult;
+import com.wjp.waicodermotherbackend.ai.model.MultiFileCodeResult;
+import com.wjp.waicodermotherbackend.model.enums.CodeGenTypeEnum;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 代码文件保存到本地
+ */
+public class CodeFileSaver {
+
+    /**
+     * 文件保存的根目录
+     */
+    private static final String FILE_SAVE_ROOT_DIR = System.getProperty("user.dir") + "/tmp/code_output";
+
+    /**
+     * 保存 HTML 网页代码
+     * @param htmlCodeResult HTML代码结果
+     */
+    public static File saveHtmlCodeResult(HtmlCodeResult htmlCodeResult) {
+        String baseDirPath = buildUniqueDir(CodeGenTypeEnum.HTML.getValue());
+        writeToFile(baseDirPath, "index.html", htmlCodeResult.getHtmlCode());
+        return new File(baseDirPath);
+    }
+
+    /**
+     * 保存 多文件 代码
+     * @param multiFileCodeResult 多文件代码结果
+     */
+    public static File saveMultiFileCodeResult(MultiFileCodeResult multiFileCodeResult) {
+        String baseDirPath = buildUniqueDir(CodeGenTypeEnum.MULTI_FILE.getValue());
+        writeToFile(baseDirPath, "index.html", multiFileCodeResult.getHtmlCode());
+        writeToFile(baseDirPath, "index.css", multiFileCodeResult.getCssCode());
+        writeToFile(baseDirPath, "index.js", multiFileCodeResult.getJsCode());
+        return new File(baseDirPath);
+    }
+
+    /**
+     * 构建文件的唯一路径: tmp/code_output/bizType_雪花ID
+     * @param bizType 业务类型
+     * @return
+     */
+    private static String buildUniqueDir(String bizType) {
+        // 构建目录名称
+        String uniqueDirName = StrUtil.format("{}_{}", bizType, IdUtil.getSnowflakeNextIdStr());
+        // 构建目录路径
+        String dirPath = FILE_SAVE_ROOT_DIR + File.separator + uniqueDirName;
+        // 创建目录
+        FileUtil.mkdir(dirPath);
+        return dirPath;
+    }
+
+    /**
+     * 保存单个文件
+     * @param dirPath 文件根目录
+     * @param fileName 文件名
+     * @param context 文件内容
+     */
+    private static void writeToFile(String dirPath,String fileName, String context) {
+        // 完整的文件路径
+        String filePath = dirPath + File.separator + fileName;
+        // 写入文件
+        FileUtil.writeString(context, filePath, StandardCharsets.UTF_8);
+    }
+
+}
