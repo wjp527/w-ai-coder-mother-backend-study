42429573b0b852621133a6539b4ffb2b5aa45c56
wjp <1060638479@qq.com>
2026-01-22 23:36:22 +0800

    微服务改造 - 网页截图服务

---
+++ wjp-ai-code-mother-microservices/wjp-ai-code-screenshot/src/main/java/com/wjp/waicodermotherbackend/WAiCodeScreenshotApplication.java
@@ -0,0 +1,11 @@
+package com.wjp.waicodermotherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WAiCodeScreenshotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WAiCodeScreenshotApplication.class, args);
+    }
+}
